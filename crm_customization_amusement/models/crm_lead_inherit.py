b0df04a91fb3c7fe406271d216c9ec646df2fd55
mukesh-tcb <mukesh.kumar@tcbinfotech.co.in>
2025-06-16 15:21:27 +0530

    Added more fields for quilified stage.

--- crm_customization_amusement/models/crm_lead_inherit.py
+++ crm_customization_amusement/models/crm_lead_inherit.py
@@ -21,6 +21,14 @@ class CrmLeadInherit(models.Model):
                             ('ivr', 'IVR')],
                             string="Lead Source",tracking=True)
     
+    visit = fields.Selection(
+        [
+            ('yes', 'Yes'),
+            ('no', 'No')
+        ]
+        , tracking=True
+    )
+    visiting_person = fields.Char(tracking=True)
     other_stakeholders_ids = fields.One2many("other.stakeholder.line",'crm_lead_id',tracking=True)
     
     customer_visit_datetime = fields.Datetime(tracking=True)
